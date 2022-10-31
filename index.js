d3755f8efa82b46c6178a7914cd6b42815a24ccd
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-31 19:34:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, name) {
+  const newArray = [];
+  for (let i of arr) {
+    newArray[
+      arr.indexOf(i)
+    ] = `Thank you, ${i}, for the wonderful surprise gift!`;
+  }
+  return newArray;
+}
+function countDown(number) {
+  for (number; number >= 0; number--) {
+    console.log(number);
+  }
+}
